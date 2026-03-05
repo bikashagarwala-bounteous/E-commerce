b18db84a1c915c71fb0fb987fa2931fecd6860a1
Bikash Agarwala <bikash.agarwala@bounteous.com>
2026-03-05 09:52:11 +0530

    chore: removed mongoose configuration

--- backend/dist/src/config/db.js
+++ backend/dist/src/config/db.js
@@ -1,18 +1,11 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
-const connectDB = async () => {
-    try {
-        await mongoose_1.default.connect(process.env.DATABASE_URL);
-        console.log("Connected to the database successfully");
-    }
-    catch (error) {
-        console.error("Error connecting to the database:", error);
-        process.exit(1);
-    }
-};
-exports.connectDB = connectDB;
+// import mongoose from "mongoose";
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DATABASE_URL as string);
+//     console.log("Connected to the database successfully");
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//     process.exit(1);
+//   }
+// };
--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL as string);
-    console.log("Connected to the database successfully");
-  } catch (error) {
-    console.error("Error connecting to the database:", error);
-    process.exit(1);
-  }
-};
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.DATABASE_URL as string);
+//     console.log("Connected to the database successfully");
+//   } catch (error) {
+//     console.error("Error connecting to the database:", error);
+//     process.exit(1);
+//   }
+// };
